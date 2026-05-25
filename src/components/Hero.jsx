c209457fd8f7e606f59555ee203f9d4687879427
ripdisweb <ripdisweb@gmail.com>
2026-05-25 12:45:36 -0700

    fix: hero background blanco

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ export default function Hero() {
         position: 'relative',
         height: '100vh',
         overflow: 'hidden',
-        background: 'var(--black)',
+        background: '#ffffff',
       }}
     >
       <style>{`
